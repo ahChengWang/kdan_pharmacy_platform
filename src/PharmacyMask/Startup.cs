cb939e18d523f693ed4ffa51eaf797824847340b
v <wang78906@yahoo.com.tw>
2021-06-09 11:36:58 +0800

    add interface

--- src/PharmacyMask/Startup.cs
+++ src/PharmacyMask/Startup.cs
@@ -20,7 +20,6 @@ namespace PharmacyMask
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSwaggerGen(g =>
@@ -55,7 +54,6 @@ namespace PharmacyMask
             services.Add(new ServiceDescriptor(typeof(DbHelperFactory), new DbHelperFactory(Configuration)));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
